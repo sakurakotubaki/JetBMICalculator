85793a060177162bf38c25b5982fefb5df56bc04
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-08-24 20:46:44 +0900

    feat: view model

--- app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.jetbmicalculator
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -10,17 +12,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetbmicalculator.ui.theme.JetBMICalculatorTheme
+import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
+    private  val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             JetBMICalculatorTheme {
                 // A surface container using the 'background' color from the theme
@@ -37,17 +42,19 @@ class MainActivity : ComponentActivity() {
 
                         // 身長
                         PinkLabeledTextField(
-                            value = "",
-                            onValueChange = {},
-                            label = "身長",
+                            value = viewModel.height,
+                            onValueChange = { viewModel.height = it},
+                            label = "身長(cm)",
                             placeholder = "170",
                         )
                         Spacer(modifier = Modifier.padding(20.dp))
 
                         // 体重
                         PinkLabeledTextField(
-                            value = "",
-                            onValueChange = {},
+                            value = viewModel.weight,
+                            onValueChange = {
+                                viewModel.weight = it
+                            },
                             label = "体重(kg)",
                             placeholder = "65",
                         )
