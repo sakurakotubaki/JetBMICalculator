76c447c19e7c8e7131dc100929834b311f5cebba
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-08-24 18:46:33 +0900

    feat: TextField

--- app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/jetbmicalculator/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.jetbmicalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetbmicalculator.ui.theme.JetBMICalculatorTheme
@@ -31,6 +30,24 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(20.dp)
                     ) {
                         Text(text = "BMI計算アプリ", fontSize = 26.sp, fontWeight = FontWeight.Bold)
+                        Spacer(modifier = Modifier.padding(30.dp))
+
+                        // 身長
+                        Text(text = "身長(cm)", color = Color(0xFFF85F64), fontWeight = FontWeight.Bold)
+                        // TextFieldを使って入力欄を作成
+                        TextField(
+                            value = "",
+                            onValueChange = { },
+                            colors = TextFieldDefaults.textFieldColors(
+                                backgroundColor = Color.Transparent// 透明にする
+                            ),
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "170")
+                            },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            singleLine = true// 1行のみ
+                        )
                     }
                 }
             }
